7cacc798ed054ee86a39d4c63f823a2ad375c9d3
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-05 14:45:24 +0600

    arithmetic operation between number data type of Ruby added

---
+++ data-types/number_dt.rb
@@ -0,0 +1,17 @@
+addition = 11+13
+puts "The addition of 11 and 13 is #{addition}"
+
+subtraction = 19-17
+puts "The subtraction of 19 and 17 is #{subtraction}"
+
+multiplication= 7 * 3
+puts "The multiplication of 7 and 3 is #{multiplication}"
+
+division= 24/6
+puts "The division of 24 and 6 is #{division}"
+
+exponentiation = 2**3
+puts "The exponentiation of 2 to the power of 3 is #{exponentiation}"
+
+modulus = 11 % 2
+puts "The modulus of 11 and 2 is #{modulus}"
