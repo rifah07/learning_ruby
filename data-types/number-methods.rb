60586fe60150916b1629bcda5a1c7240a1b93d84
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-06 23:14:23 +0600

    useful number methods added

---
+++ data-types/number-methods.rb
@@ -0,0 +1,16 @@
+num1= 6
+num2= 7
+
+result1= num1.even?
+result2= num1.odd?
+
+result3= num2.even?
+result4= num2.odd?
+
+puts "Results for #{num1}:"
+puts "Even? #{result1}"
+puts "Odd? #{result2}"
+
+puts "Results for #{num2}:"
+puts "Even? #{result3}"
+puts "Odd? #{result4}"
