a0f9c26d471292cdda780a3821dade1846e82aba
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-06 23:53:30 +0600

    substrings example added

--- data-types/string/string-dt.rb
+++ data-types/string/string-dt.rb
@@ -1,31 +1,5 @@
 word1= "Assalamu Alaikum"
 word2= "everyone"
 
-sentence1= word1 + " " + word2 + "!"
-
-#Concatenation
-
-sentence1= word1 + " " + word2 + "!"
-
-
-
-puts "Concatenations in different ways:"
-puts "Concatenation using + operator:"
-puts sentence1
-puts ""
-
-sentence2= word1 << " " << word2 << "!"
-puts "Using << operator:"
-puts sentence2
-puts "word1 after << operator: #{word1}"
-puts ""
-
-word3= "Assalamu Alaikum"
-
-sentence3= word3.concat(" ",word2, "!")
-puts "Using concat method:"
-puts sentence3
-puts "word3 after concat method: #{word3}"
-puts ""
-
-puts "Note: The << operator".concat(" and concat method modify the original string, ").concat(" while the + operator creates a new string without modifying the original strings.")
+puts word1
+puts word2
---
+++ data-types/string/substring.rb
@@ -0,0 +1,11 @@
+subs1= "hello"[0]
+subs2= "hello"[0..1]
+subs3= "hello"[0, 4]
+subs4= "hello"[-1]
+subs5= "hello"[-2..-1]
+
+puts "The first character of 'hello' is: #{subs1}"
+puts "The first two characters of 'hello' are: #{subs2}"
+puts "The first four characters of 'hello' are: #{subs3}"
+puts "The last character of 'hello' is: #{subs4}"
+puts "The last two characters of 'hello' are: #{subs5}"
