ebb5a7a5e56f4035fce9061c570d3ad918a87e63
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-07 00:10:56 +0600

    interpolation added

---
+++ data-types/string/interpolation.rb
@@ -0,0 +1,7 @@
+# Interpolation allows to evaluate a string that contains placeholder variables
+# without "" / double quotes strings are not interpolated, and escape characters are not processed.
+
+name= "John"
+
+puts "Hello #{name}" # String interpolation
+puts 'Hello #{name}' # No interpolation, prints the string as is
