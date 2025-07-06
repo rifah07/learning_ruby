717ec8893da9acf1b634e940d26d44545be66ef7
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-07 00:19:21 +0600

    multiple methods of Ruby added

---
+++ data-types/string/methods.rb
@@ -0,0 +1,27 @@
+puts "hello".capitalize
+puts "hello".upcase
+puts "HELLO".downcase
+
+puts "hello".include?("lo")
+puts "hello".include?("li")
+
+puts "hello".empty? 
+puts "".empty?
+
+puts "hello".length 
+
+puts "hello".reverse
+
+puts "hello world".split
+puts "hello".split("")
+
+puts " hello, world   ".strip
+
+puts "he77o".sub("7", "l") 
+puts "he77o".gsub("7", "l")
+puts "hello world".gsub("world", "ruby")
+
+puts "hello".insert(-1, " dude")   
+puts "hello world".delete("l") 
+
+puts "!".prepend("hello, ", "world")
