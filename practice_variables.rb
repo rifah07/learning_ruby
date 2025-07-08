d90ee6918eec62734bc1a45eade4cac8d44b98d5
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-08 10:38:24 +0600

    updated

--- practice_variables.rb
+++ practice_variables.rb
@@ -25,9 +25,23 @@ print "d: #{d}\n"
 =end
 
 # Practice 3
+=begin
 def test(b)
   b.map {|letter| "I like the letter: #{letter}"}
 end
 
 a = ['a', 'b', 'c']
 puts test(a)
+=end
+
+# practice 4
+puts "powerball" =~ /b/ # =~ checks if the pattern matches the string and returns the index of the first match or nil if no match is found
+puts "powerball" =~ /z/ # returns nil since 'z' is not in the string
+
+# practice 5
+puts "powerball".match(/b/) # match returns a MatchData object if the pattern matches, or nil if no match is found
+puts "powerball".match(/z/) # returns nil since 'z' is not in the string
+
+# practice 6
+puts "powerball".include?("b") # include? checks if the string contains the specified substring and returns true or false
+puts "powerball".include?("z") # returns false since 'z' is not in the string
