c3e0f2426b6bb80d8ae3d62072d668fa74ceadcc
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-08 10:27:42 +0600

    array variable added

---
+++ practice_variables.rb
@@ -0,0 +1,16 @@
+# practice 1
+=begin
+a = "hi there"
+b = a
+a = "not here"
+puts a
+puts b
+=end
+
+# practice 2
+a = [1, 2, 3, 3]
+b = a
+c = a.uniq
+print "a: #{a}\n"
+print "b: #{b}\n"
+print "c: #{c}\n"
