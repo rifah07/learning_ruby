ea8ae877308317d69454b491b5fccb5a38f8e04d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-11 22:20:38 +0600

    example added of exception handling

---
+++ exception-handling/example.rb
@@ -0,0 +1,11 @@
+names = ['bob', 'joe', 'steve', nil, 'frank']
+
+names.each do |name|
+  begin
+    puts "#{name}'s name has #{name.length} letters in it."
+  rescue
+    puts "Something went wrong!"
+  end
+end
+
+#for 'nil' exception handles as it is nit a name
