b083a0655f6833b633b047398119184d6410b518
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-13 10:30:17 +0600

    exercise 2 solved

--- variable/exercise/what-is-output.rb
+++ variable/exercise/what-is-output.rb
@@ -5,3 +5,10 @@ end
 execute { puts "Hello from inside the execute method!" }
 
 #Nothing is printed to the screen because the block is never activated with the .call method. The method returns a Proc object.
+
+# Now this should worK
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
