3be00d65b0abdbfaa888084246d9c2f5d1354ca1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-13 10:33:19 +0600

    a new case added in exercise 2

--- variable/exercise/what-is-output.rb
+++ variable/exercise/what-is-output.rb
@@ -12,3 +12,16 @@ def execute(&block)
 end
 
 execute { puts "Hello from inside the execute method!" }
+
+# This will ot work
+def execute(block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+=begin
+see here '&' is missing in the argument list of the method definition. '&' allows a block to be passed as a parameter.
+The block is not being passed as a Proc object, so it cannot be called with .call. 
+Instead, it is treated as a regular argument, which does not have a call method.
+=end
