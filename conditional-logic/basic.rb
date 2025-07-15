ccd88c78fd1cc966fe23ee4aa82b29ee115d492f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-15 22:38:32 +0600

    case statement added in new file

--- conditional-logic/basic.rb
+++ conditional-logic/basic.rb
@@ -8,7 +8,6 @@ end
 # puts "Hot diggity damn, 1 is less than 2" if 1 < 2
 
 
-=begin
 attack_by_land = false
 attack_by_sea = false
 
@@ -18,19 +17,4 @@ elsif attack_by_sea == true
   puts "release the shark"
 else
   puts "release Kevin the octopus"
-end
-=end
-
-grade = 'F'
-
-case grade
-when 'A'
-  puts "You're a genius"
-  future_bank_account_balance = 5_000_000
-when 'D'
-  puts "Better luck next time"
-  can_i_retire_soon = false
-else
-  puts "'YOU SHALL NOT PASS!' -Gandalf"
-  fml = true
 end
