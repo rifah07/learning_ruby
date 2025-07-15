d05eb33e2aef6a1fd72dd6f821a4fce1e9f3b1a6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-15 22:27:08 +0600

    basic conditional logic added

---
+++ conditional-logic/basic.rb
@@ -0,0 +1,25 @@
+=begin
+ if 1 < 2
+  puts "Hot diggity, 1 is less than 2!"
+end
+=end
+
+# the above block can be written as a one-liner
+# puts "Hot diggity damn, 1 is less than 2" if 1 < 2
+
+
+=begin
+
+=end
+
+
+attack_by_land = false
+attack_by_sea = false
+
+if attack_by_land == true
+  puts "release the goat"
+elsif attack_by_sea == true
+  puts "release the shark"
+else
+  puts "release Kevin the octopus"
+end
