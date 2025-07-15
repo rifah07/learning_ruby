8d4384c168f7ad8e8ac67d3144622322f0c79801
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-15 22:19:28 +0600

    2 new types of printing p and pp added

--- variable/variables_InOutPut.rb
+++ variable/variables_InOutPut.rb
@@ -8,3 +8,12 @@ print "What is your name? " # print does not add a newline character at the end
 name1 = gets.chomp
 
 puts "Hello #{name1}!" #now added chomp to remove the newline character
+
+# p is a method that shows a more “raw” version of an object.
+ p "Ruby Is Cool"
+
+ # there is anothet method, pp means preety printing
+ #This is like p, but it prints big hashes & arrays in a nicer way.
+
+pp [1, 2, 3, 4, 5]
+pp({name: "John", age: 30, city: "New York"})
