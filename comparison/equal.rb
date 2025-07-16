024ab73c1c5601342e66907ed27c385a14d7c5a9
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-16 18:57:13 +0600

    updated

--- comparison/equal.rb
+++ comparison/equal.rb
@@ -11,3 +11,17 @@ if num1 == num2
 else 
   puts "Numbers are not equal"
 end
+
+if num1.to_s == num2.to_s
+  puts "Numbers are equal when converted to string #{num1} and #{num2}"
+else 
+  puts "Numbers are not equal when converted to string #{num1} and #{num2}"
+end
+
+#  to use "==" to compare, both values should be of same data type
+# if one is string and other is integer, it will not work as expected"
+if num1.to_s == num2
+  puts "Numbers are equal when #{num1} compared as string"
+else 
+  puts "Numbers are not equal when first number #{num1} compared as strings"
+end
