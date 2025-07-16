65fa3bb1f82814396c0d93bc82033c6df6ae4333
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-16 18:43:36 +0600

    then keyword added

--- conditional-logic/if-else.rb
+++ conditional-logic/if-else.rb
@@ -9,3 +9,8 @@ elsif input == 2
 else 
   puts "Input is not 1 or 2"
 end
+
+# Must use "then" keyword when using 1-line syntax
+puts "Input is One" if input == 1
+puts "Input is Two" if input == 2
+puts "Input is not 1 or 2" if input != 1 && input != 2
