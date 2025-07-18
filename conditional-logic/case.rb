3b86a73bb01852949f973078e3893784d1ebd0a3
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-18 10:09:43 +0600

    case refactored

--- conditional-logic/case.rb
+++ conditional-logic/case.rb
@@ -11,3 +11,19 @@ else
   puts "'YOU SHALL NOT PASS!' -Gandalf"
   fml = true
 end
+
+# You can also save the result of a case statement into a variable
+
+answer = case grade
+when 'A'
+  "You're a genius"
+  future_bank_account_balance = 5_000_000
+when 'D'
+  "Better luck next time"
+  can_i_retire_soon = false
+else
+  "'YOU SHALL NOT PASS!' -Gandalf"
+  fml = true
+end
+
+puts answer
