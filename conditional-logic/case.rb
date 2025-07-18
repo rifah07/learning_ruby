bf1f7c12c0a2a94f3675f7393f93f5b51edf6882
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-18 10:14:25 +0600

    case updated

--- conditional-logic/case.rb
+++ conditional-logic/case.rb
@@ -1,4 +1,4 @@
-grade = 'F'
+grade = 'A'
 
 case grade
 when 'A'
@@ -10,7 +10,7 @@ when 'D'
 else
   puts "'YOU SHALL NOT PASS!' -Gandalf"
   fml = true
-end
+end 
 
 # You can also save the result of a case statement into a variable
 
@@ -19,6 +19,25 @@ when 'A'
   "You're a genius"
   future_bank_account_balance = 5_000_000
 when 'D'
+  "Better luck next time"
+  can_i_retire_soon = false
+else
+  "'YOU SHALL NOT PASS!' -Gandalf"
+  fml = true
+end
+
+puts answer
+
+
+
+# There's a second form of the case statement that doesn't take an argument. 
+# But this only prints last assigned value not the statement as before
+
+answer = case
+when grade == 'A'
+  "You're a genius"
+  future_bank_account_balance = 5_000_000
+when grade == 'D'
   "Better luck next time"
   can_i_retire_soon = false
 else
