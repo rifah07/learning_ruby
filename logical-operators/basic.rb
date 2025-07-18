e8c3629370e3b4b14c725df9dc45b14f47426417
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-18 11:06:42 +0600

    updated last one

--- logical-operators/basic.rb
+++ logical-operators/basic.rb
@@ -1,3 +1,11 @@
+puts 3 || 'foo'   # last evaluated operand is 3
+
+puts nil || 'foo'  # last evaluated operand is foo
+
+puts nil && 'foo'  # last evaluated operand is nil
+
+puts 3 && 'foo'    # last evaluated operand is 'foo'
+
 # The && and || logical operators, as you'll recall, use short-circuit evaluation.
 # These operators work with truthy and falsy values too, and they can also return truthy values instead of boolean values.
 # When using && and ||, the return value is always the value of the operand evaluated last
@@ -7,3 +15,7 @@ str2= 'string_2'
 
 output = str1 || str2
 puts output
+
+#  using a string value as though it is a boolean isn't the clearest way to write your code.
+# It may even look like a mistake to another programmer who is trying to track down a bug. In some strange cases,
+# it may even be a mistake.
