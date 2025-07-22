1bd963a6290078ac814ed393d00fe5326e648d6f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-22 20:08:27 +0600

    next added

--- all_loop/loop.rb
+++ all_loop/loop.rb
@@ -9,4 +9,16 @@ loop do
   break if i == 10
 end
 
+# We can use the keyword next to skip the rest of the current iteration and start executing the next iteration.
 
+i = 0
+loop do
+  i = i + 2
+  if i == 4
+    next        # skip rest of the code in this iteration
+  end
+  puts i
+  if i == 10
+    break
+  end
+end
