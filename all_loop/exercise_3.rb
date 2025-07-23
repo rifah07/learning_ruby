ffc909727481f40c144127f409443fe6b6136c2b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-23 11:10:04 +0600

    exercise 3 solved

---
+++ all_loop/exercise_3.rb
@@ -0,0 +1,12 @@
+# Write a method that counts down to zero using recursion.
+
+def countdown(n)
+  if n < 0
+    puts "COUNTDOWN FINISHED"
+  else
+    puts n
+    countdown(n - 1)
+  end
+end
+
+countdown(5)
