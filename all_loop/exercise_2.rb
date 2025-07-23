e58a95aebfcfa1ca299e81c66ac681c5d2cd1623
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-23 11:08:37 +0600

    question added in exercise 2

--- all_loop/exercise_2.rb
+++ all_loop/exercise_2.rb
@@ -1,3 +1,6 @@
+# Write a while loop that takes input from the user, performs an action,
+# and only stops when the user types "STOP". Each loop can get info from the user.
+
 x = ""
 while x != "STOP"
     x = gets.chomp
