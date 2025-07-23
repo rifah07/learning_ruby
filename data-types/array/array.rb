58f747fee3051ad8ecd82e248c3eb2041fbef317
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-23 11:12:55 +0600

    array added

---
+++ data-types/array/array.rb
@@ -0,0 +1,10 @@
+# array literal-> []
+# array methods: .each, .map, .select, .reject, .find, .include?, .sort, .reverse, .join
+# array indexing: array[index], array[start_index..end_index]
+# array length: array.length or array.size
+# nested arrays: array_of_arrays = [[1, 2], [3, 4]]
+# multidimensional arrays: matrix-like structures
+# array destructuring: a, b = [1, 2]
+# array concatenation: array1 + array2
+# array slicing: array[start_index, length]
+# array comparison: ==, !=, <=> (spaceship operator)
