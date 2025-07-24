659d1a07be265d2e8e5d4e45a4366624cad9b4f6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-24 06:20:56 +0600

    included how to remove elements from array

--- data-types/array/add-remove.rb
+++ data-types/array/add-remove.rb
@@ -9,3 +9,10 @@ num_array << 8
 puts "The second approach to add elements to an array is using the shovel operator."
 puts "This operator is used to append elements to the end of an array."
 puts num_array.inspect  #=> [1, 2, 3, 4, 5, 6, 7, 8]
+
+# remove elements from the end of an array
+puts "The first approach to remove elements from an array is using the pop method."
+num_array.pop
+puts num_array.inspect  #=> [1, 2, 3, 4, 5, 6, 7]
+num_array.pop(2)
+puts num_array.inspect  #=> [1, 2, 3, 4, 5]
