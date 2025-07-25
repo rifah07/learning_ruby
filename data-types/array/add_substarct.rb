3fbbe1a7e31894bfea092e8cb6d275ec9ec7abe0
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-25 12:27:06 +0600

    array subtraction added

--- data-types/array/add_substarct.rb
+++ data-types/array/add_substarct.rb
@@ -5,3 +5,7 @@ b = [3, 4, 5]
 
 puts a + b         #=> [1, 2, 3, 3, 4, 5]
 puts a.concat(b)   #=> [1, 2, 3, 3, 4, 5]
+
+# array subtraction returns a new array
+# using the - operator or the subtract method
+puts [1, 1, 1, 2, 2, 3, 4] - [1, 4]  #=> [2, 2, 3]
