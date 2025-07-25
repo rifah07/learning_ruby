05acf9bbb8ac93dc8c6b2dab53fced6364b27e9b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-25 12:25:38 +0600

    array addition or concetination added

---
+++ data-types/array/add_substarct.rb
@@ -0,0 +1,7 @@
+# adding 2 arrays returns a new array
+# using the + operator or the concat method
+a = [1, 2, 3]
+b = [3, 4, 5]
+
+puts a + b         #=> [1, 2, 3, 3, 4, 5]
+puts a.concat(b)   #=> [1, 2, 3, 3, 4, 5]
