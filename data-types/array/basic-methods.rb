ad59b9387c41e724b317492da2b882d3a4605a20
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-25 12:30:04 +0600

    updated previous one

--- data-types/array/basic-methods.rb
+++ data-types/array/basic-methods.rb
@@ -4,3 +4,10 @@ puts [1, 2].empty?           #=> false
 
 puts [1, 2, 3].length        #=> 3
 
+puts [1, 2, 3].reverse       #=> [3, 2, 1]
+
+puts [1, 2, 3].include?(3)   #=> true
+puts [1, 2, 3].include?("3") #=> false
+
+puts [1, 2, 3].join          #=> "123"
+puts [1, 2, 3].join("-")     #=> "1-2-3"
