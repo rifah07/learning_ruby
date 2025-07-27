dc8e360861bbf2a1aa627eae0ed6a8a9094d9f22
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-27 20:42:11 +0600

    added reverse_each iterator

--- data-types/array/itrartion.rb
+++ data-types/array/itrartion.rb
@@ -1,3 +1,9 @@
 arr = [1, 2, 3, 4, 5]
 arr.each {|a| print a -= 10, " "} # this does not change the arr- array
 puts arr
+
+# another iterator is reverse_each which will iterate over the elements in the array in reverse order.
+words = %w[first second third fourth fifth sixth]
+str = ""
+words.reverse_each {|word| str += "#{word} "}
+p str #=> "sixth fifth fourth third second first "
