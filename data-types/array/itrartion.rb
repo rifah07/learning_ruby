a3f6b478e0adb58d33f5cc513f3aa0bc5c80b097
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-28 23:33:58 +0600

    map method added

--- data-types/array/itrartion.rb
+++ data-types/array/itrartion.rb
@@ -6,4 +6,14 @@ puts arr
 words = %w[first second third fourth fifth sixth]
 str = ""
 words.reverse_each {|word| str += "#{word} "}
-p str #=> "sixth fifth fourth third second first "
+p str #=> "sixth fifth fourth third second first "#
+ 
+# The map method can be used to create a new array based on the original array,
+# but with the values modified by the supplied block
+puts "Using map and map! methods"
+arr_1 = [1, 2, 3, 4, 5]
+puts "Array before map: #{arr_1}" #=> [1, 2, 3, 4, 5]
+puts "Using map method"
+puts arr_1.map {|a| 2*a}     #=> [2, 4, 6, 8, 10]
+puts arr_1.map! {|a| a**2}   #=> [1, 4, 9, 16, 25]
+puts "Array after map!: #{arr_1}" #=> [1, 4, 9, 16, 25]
