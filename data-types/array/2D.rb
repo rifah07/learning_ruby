f337ca23e9b2b5d513f847431dc065d5de737d70
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-01 13:29:22 +0600

    converted 2D to 1D array

--- data-types/array/2D.rb
+++ data-types/array/2D.rb
@@ -7,3 +7,6 @@ puts users[0][0]  # Output: 1
 puts users[0][1]  # Output: Peter
 puts users[1][0]  # Output: 2
 puts users[1][1]  # Output: Steven
+
+# convert to regular array
+puts "The converted regular array is: #{users.flatten}"
