5ecc263a2d55a2cbdb26bd6de20bff892263de58
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-02 20:47:51 +0600

    each vs map added

---
+++ data-types/array/each_vs_map.rb
@@ -0,0 +1,21 @@
+#  Where it really differs from each is the returned value.
+# map creates and returns a new array containing the values returned by the block
+a= [1,2,3,4,5]
+puts "Main array: #{a}"
+b= a.each do |i|
+  puts "Inside each: #{i * 2}"
+end
+
+puts "After each: #{b}"
+puts a==b
+
+# In this case, b will be the original array a, not a new one.
+# To create a new array with the results, use map instead of each.
+# Using map to create a new array with doubled values
+# The original array a remains unchanged.
+c = a.map do |i|
+  i * 2
+end
+puts "After map: #{c}"
+puts a==c
+puts "Original array: #{a}"
