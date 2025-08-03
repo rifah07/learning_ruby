fdfad48684f2cd334dcb96e9ce19056e5077fdf6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-03 13:04:23 +0600

    sample method added

---
+++ data-types/array/random_values.rb
@@ -0,0 +1,7 @@
+arr = %w{a b c d e f g h i j}
+
+puts arr.sample
+puts arr.sample(3).inspect
+# The sample method comes in two forms. In the first form, we select a random element.
+# In the second form, we select n random elements from the array.
+
