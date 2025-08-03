9908930fed36c91451a08d31b68c97cc0665bee3
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-03 12:25:18 +0600

    leetcode solution added

---
+++ data-types/array/exercises/leetcode.rb
@@ -0,0 +1,39 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+
+# 217
+# this got TLA:
+def contains_duplicate(nums)
+    nums.sort!
+    n= nums.length
+    num2= []
+
+    (0..n-1).each do |i|
+        temp = nums[i]
+        if num2.include?(temp)
+         return true
+        end
+        num2 << temp
+    end
+
+  false
+    
+end
+
+
+# this is accepted:
+def contains_duplicate(nums)
+    num2= Set.new
+
+    nums.each do |num|
+        if num2.include?(num)
+         return true
+        end
+        num2.add(num)
+    end
+  false    
+end
+
+
+nums = [1, 2, 3, 4,5,6,6]
+puts contains_duplicate(nums)
