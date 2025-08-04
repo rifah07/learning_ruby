dc34956b32ff91a14c493d068543cf424cb9d3a3
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-04 17:22:37 +0600

    string key added in hash

--- data-types/array/hash.rb
+++ data-types/array/hash.rb
@@ -1,3 +1,6 @@
+# In Ruby, key? is a Hash method that checks whether a specific key exists in the hash.
+# It returns a boolean value (true or false).
+
 # create a hash
 person = {name: "Alice", age: 24, city: "California"}
 
@@ -8,3 +11,12 @@ puts person.key?(:country)
 puts "Check if value exist"
 puts person.value?("Alice")
 puts person.value?("New York")
+
+# With string keys
+scores= {"math" => 95, "science" => 94, "english" => 96}
+puts "Check if key exist in scores"
+puts scores.key?("math")
+puts scores.key?("history")
+puts "Check if value exist in scores"
+puts scores.value?(95)
+puts scores.value?(100)
