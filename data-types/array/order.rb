7997e788fa5b1ea54490539fe10a3f77ca4f34ac
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-06 18:40:35 +0600

    comments added for better readability

--- data-types/array/order.rb
+++ data-types/array/order.rb
@@ -6,6 +6,7 @@ puts "Actual planets array: #{planets.inspect}" #=> ["mercury", "venus", "earth"
 
 puts "Using sort method on numbers array #{numbers.sort.inspect}" #=> [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
 puts "Using sort method on planets array #{planets.sort.inspect}" #=> ["earth", "jupiter", "mars", "mercury", "neptune", "saturn", "uranus", "venus"]
+# The sort method alphabetically sorts the array elements.
 
 puts "Using sort_by method on numbers array #{numbers.sort_by {|n| n}.inspect}" #=> [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 puts "Using sort_by method on planets array #{planets.sort_by {|p| p}.inspect}" #=> ["earth", "jupiter", "mars", "mercury", "neptune", "saturn", "uranus", "venus"]
@@ -16,13 +17,10 @@ puts "Using reverse method on numbers array #{numbers.reverse.inspect}" #=> [5, 
 puts "Using reverse method on planets array #{planets.reverse.inspect}" #=> ["neptune", "uranus", "saturn", "jupiter", "mars", "earth", "venus", "mercury"]
 puts "Using reverse! method on numbers array #{numbers.reverse!.inspect}" #=> [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 puts "Using reverse! method on planets array #{planets.reverse!.inspect}" #=> ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
-
-puts "Using uniq method on numbers array #{numbers.uniq.inspect}" #=> [1, 2, 3, 4, 5]
-puts "Using uniq method on planets array #{planets.uniq.inspect}" #=> ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
-puts "Using uniq! method on numbers array #{numbers.uniq!.inspect}" #=> [1, 2, 3, 4, 5]
-puts "Using uniq! method on planets array #{planets.uniq!.inspect}" #=> ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+# The reverse method returns a new array with all elements in a reverse order.
 
 puts "Using shuffle method on numbers array #{numbers.shuffle.inspect}" #=> [3, 1, 5, 2, 4, 1, 2, 4, 5, 3]
 puts "Using shuffle method on planets array #{planets.shuffle.inspect}" #=> ["venus", "neptune", "jupiter", "mars", "saturn", "uranus", "earth", "mercury"]
 puts "Using shuffle! method on numbers array #{numbers.shuffle!.inspect}" #=> [2, 4, 5, 1, 3, 1, 2, 4, 5, 3]
 puts "Using shuffle! method on planets array #{planets.shuffle!.inspect}" #=> ["mars", "neptune", "venus", "jupiter", "saturn", "uranus", "earth", "mercury"]
+# The shuffle method randomly reorganizes the array elements.
