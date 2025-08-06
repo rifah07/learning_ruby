d2afca3e969d9a06a76db29f9dcdf58bd96dda71
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-06 18:23:59 +0600

    set operations added

---
+++ data-types/array/set_operations.rb
@@ -0,0 +1,14 @@
+A = [1, 2, 3, 4, 5, 5, 6]
+B = [5, 6, 6, 7, 8, 9, 10]
+
+union = A | B
+intersect = A & B
+diff1 = A - B
+diff2 = B - A
+symmetric_diff = (A - B) | (B - A)
+
+puts "Union of A and B: #{union.inspect}"  #=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+puts "Intersection of A and B: #{intersect.inspect}"  #=> []
+puts "Difference A - B: #{diff1.inspect}"  #=> [1, 2, 3, 4, 5]
+puts "Difference B - A: #{diff2.inspect}"  #=> [6, 7, 8, 9, 10]
+puts "Symmetric Difference of A and B: #{symmetric_diff.inspect}"  #=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
