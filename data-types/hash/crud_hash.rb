0381dc0e9eb8803e116ed9a0b091eeaa2d379b70
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-09 16:49:34 +0600

    adding and removing in hash added

---
+++ data-types/hash/crud_hash.rb
@@ -0,0 +1,23 @@
+shoes = {
+  "summer" => "sandals",
+  "winter" => "boots"
+}
+print "The shoe for summer: "
+puts shoes.fetch("summer")
+print "The shoe for hiking: "
+puts shoes.fetch("hiking", "hiking boots") # use fetch with default
+
+# add values
+shoes["fall"] = "sneakers"
+print "Shoe for fall: "
+puts shoes["fall"]
+
+print "Shoes hash now: "
+p shoes
+
+# remove data
+
+shoes.delete("winter")
+print "Shoes hash after removing winter: "
+p shoes
+p shoes["winter"]
