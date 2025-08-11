30d0d2ffd602f19db68d3fc87489c80ccd9ec780
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-11 09:22:11 +0600

    value? method added for an exercise

--- data-types/hash/exercise/launch_schl_2.rb
+++ data-types/hash/exercise/launch_schl_2.rb
@@ -7,3 +7,15 @@ puts "Names:"
 p names
 
 p person[:name]
+
+# another little exercise
+# What method could you use to find out if a Hash contains a specific value in it?
+# Write a program that verifies that the value is within the hash.
+
+if person.has_value?('Bob')
+  puts "Bob is one of the values in the person hash."
+else
+  puts "Bob is not one of the values in the person hash."
+end
+
+
