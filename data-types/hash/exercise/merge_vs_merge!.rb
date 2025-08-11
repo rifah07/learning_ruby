b4be1041ebfa5841332ac20e115e0776f735760c
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-11 09:14:11 +0600

    merge vs merge added

---
+++ data-types/hash/exercise/merge_vs_merge!.rb
@@ -0,0 +1,10 @@
+# The difference is merge! modifies permanently, while merge does not.
+
+cat = {name: "whiskers"}
+weight = {weight: "10 lbs"}
+puts cat.merge(weight)
+puts cat                  # => {:name=>"whiskers"}
+puts weight               # => {:weight=>"10 lbs"}
+puts cat.merge!(weight)
+puts cat                  # => {:name=>"whiskers", :weight=>"10 lbs"}
+puts weight               # => {:weight=>"10 lbs"}
