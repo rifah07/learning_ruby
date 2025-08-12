c21b793e58432103ea45dde710f66ea0e8ed28ed
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-12 10:54:44 +0600

    function and method added

---
+++ methods/functions_vs_methods.rb
@@ -0,0 +1,25 @@
+=begin
+You'll often write the same code in different parts of your program.
+Wouldn't it be nice if you could use the same code many times without
+writing it again and again? That's what methods do! Methods let you give a name
+to a piece of code. Then you can use that code anywhere in your program just by calling its name.
+This idea is called DRY programming: Don't Repeat Yourself.
+When you reuse code instead of copying it everywhere, your program becomes easier to read and work with.
+=end
+
+=begin
+According to The Ruby Programming Language book:
+Many languages distinguish between functions,
+which have no associated object, and
+methods, which are invoked on a receiver object.
+Because Ruby is a purely object-oriented language,
+all methods are true methods and are associated with at least one object. 
+=end
+
+=begin
+Basically, because everything in Ruby is an object,
+Ruby only has methods, not functions. With that established,
+we know that when we’re talking about Ruby, “methods” and “functions” refer to the same thing.
+- The Odin Project
+=end
+
