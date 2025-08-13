9dbdfa675a69a2dda947c13ebc2cad871652f397
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-14 00:09:33 +0600

    previous was not bang method, added now

--- methods/bang.rb
+++ methods/bang.rb
@@ -2,3 +2,8 @@ whisper = "HELLO EVERYBODY"
 
 puts whisper.downcase #=> "hello everybody"
 puts whisper #=> "HELLO EVERYBODY"
+
+# bang method
+puts whisper.downcase! #=> "hello everybody"
+puts whisper #=> "hello everybody"
+
