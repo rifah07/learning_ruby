8d93874d4ab6cdcc35efc15f2d1a16b8b99a15b5
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-14 00:07:05 +0600

    predicate method added

---
+++ methods/predicate.rb
@@ -0,0 +1,11 @@
+=begin
+Ruby includes certain built-in methods that end with a question mark (?),
+like even?, odd?, or between?. This naming pattern identifies predicate methods
+- a Ruby convention used to indicate methods that return Boolean values (true or false).
+=end
+
+puts 5.even?  #=> false
+puts 6.even?  #=> true
+puts 17.odd?  #=> true
+
+puts 12.between?(10, 15)  #=> true
