a69ff6b9bfb603f36ad12fb05041c84fc9910ae6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-14 00:01:33 +0600

    updated

--- methods/chain.rb
+++ methods/chain.rb
@@ -1,8 +1,13 @@
 #One of the magical properties of methods that allows to write very concise code is 
 # being able to chain methods together. This can be done using Ruby’s built-in methods
-## or with methods that you create. -the odin project
+# or with methods that you create. -the odin project
 
 phrase = ["be", "to", "not", "or", "be", "to"]
 
 puts phrase.reverse.join(" ").capitalize
 #=> "To be or not to be"
+
+# In this example, we are using three methods:
+# - reverse: reverses the order of the elements in the array
+# - join: combines the elements of the array into a single string, with a space in between each element
+# - capitalize: capitalizes the first letter of the string
