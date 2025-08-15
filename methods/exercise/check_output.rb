5698fc5b76b869ebf71a8b443c478a9e9b5886e4
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-15 19:50:42 +0600

    slightly updated

--- methods/exercise/check_output.rb
+++ methods/exercise/check_output.rb
@@ -9,10 +9,20 @@ say("I'm fine")
 #say(5) # TypeError
 
 
+=begin
 a = 5
 
 def some_method
   a = 3
 end
 
+puts a # 5 cause a is global variable and it is printing  outside the block
+=end
+
+def some_method(number)
+  number = 7 # this is implicitly returned by the method
+end
+
+a = 5
+some_method(a)
 puts a # 5 cause a is global variable and it is printing  outside the block
