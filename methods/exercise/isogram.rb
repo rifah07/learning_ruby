fbcec2966ae1c1e3b006cfc746419806c18d69a8
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-17 23:46:56 +0600

    isogram code corrected

--- methods/exercise/isogram.rb
+++ methods/exercise/isogram.rb
@@ -1,11 +1,12 @@
+#isogram (a word that has no repeating letters) 
 def isogram (word)
   len = word.length
-  word_to_array = word.downcase.split
+  word_to_array = word.downcase.split("")
   len2 = word_to_array.uniq.length
   len == len2
 end
 
-puts "Isogram check for 'word': #{isogram('word')}" #=> false
-puts "Isogram check for 'isogram': #{isogram('isogram')}" #=> false
-puts "Isogram check for 'odin': #{isogram('odin')}" #=> false
+puts "Isogram check for 'word': #{isogram('word')}" #=> true
+puts "Isogram check for 'isogram': #{isogram('isogram')}" #=> true
+puts "Isogram check for 'odin': #{isogram('odin')}" #=> true
 puts "Isogram check for 'Sleep': #{isogram('Sleep')}" #=> false
