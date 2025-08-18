57959223a186e9aa7d664325ed77b920d77806cf
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-18 12:50:21 +0600

    used match method instead of =~ operator

--- regex/boolean_regex.rb
+++ regex/boolean_regex.rb
@@ -8,3 +8,17 @@ end
 
 has_a_b?("powerball")
 has_a_b?("powerful")
+
+# On top of the =~ operator, we can use the match method to perform regex comparisons as well.
+# This method returns a MatchData object that describes the match or nil if there is no match.
+
+def has_a_b_2?(string)
+  if /b/.match(string)
+    puts "Found a 'b'!"
+  else
+    puts "No 'b' found."
+  end
+end
+
+has_a_b_2?("powerball")
+has_a_b_2?("powerful")
