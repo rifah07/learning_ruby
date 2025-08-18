24cb46e652eb55e8280e0249b72fda775ea020b1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-18 12:42:21 +0600

    regex definition added

---
+++ regex/regex.rb
@@ -0,0 +1,5 @@
+# Regex stands for regular expression. A regular expression is a sequence 
+# of characters that form pattern matching rules, and is then applied to a string to look for matches.
+
+# Example: Check if the pattern "ss" appears in the string "Mississippi".
+
