64b33c5ab1c592e1826a667ab562367ecc1a5e46
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-19 12:05:29 +0600

    another exercise added

---
+++ regex/check_otput.rb
@@ -0,0 +1,15 @@
+def execute(&block)
+  block
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+# did not print anything cause the block is not being called
+
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+# now it works after block.call
