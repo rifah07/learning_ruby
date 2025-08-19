62e912821a5a1dea2bd6ed94a251d32cd7456929
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-19 12:06:45 +0600

    Exception handling updated

--- exception-handling/basic.rb
+++ exception-handling/basic.rb
@@ -10,3 +10,6 @@ rescue
   # do this if operation fails
   # for example, log the error
 end
+
+# Exception handling is a structure used to handle the possibility of an error occurring in a program.
+# It is a way of handling the error by changing the flow of control without exiting the program entirely. - The LAUNCH SCHOOL
