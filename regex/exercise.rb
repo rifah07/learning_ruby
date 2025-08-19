29a50d64a30d8607f81331849e90dd6a05bafffb
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-19 12:03:42 +0600

    an exercise solved of regex

---
+++ regex/exercise.rb
@@ -0,0 +1,14 @@
+# Write a program that checks if the sequence of characters "lab" exists in strings.
+# If it does exist, print out the word.
+
+def contains_lab?(string)
+  if /lab/.match(string)
+    puts string
+  end
+end
+
+contains_lab?("laboratory")
+contains_lab?("experiment")
+contains_lab?("Pans Labyrinth")
+contains_lab?("elaborate")
+contains_lab?("polar bear")
