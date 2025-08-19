0f142dd430af06640bdc0b7e393c1e369bba629b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-19 12:08:55 +0600

    updated

--- regex/check_otput.rb
+++ regex/check_otput.rb
@@ -12,4 +12,12 @@ end
 
 execute { puts "Hello from inside the execute method!" }
 
-# now it works after block.call
+# now it works after block.call\
+
+
+def execute(block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
+# this throws error as block-method parameter is not written as &block
