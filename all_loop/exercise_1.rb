6186ea817177caef81e5df6e7602dcd4fb57eeec
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-20 20:48:08 +0600

    a theory added

--- all_loop/exercise_1.rb
+++ all_loop/exercise_1.rb
@@ -2,3 +2,10 @@ x = [1, 2, 3, 4, 5]
 x.each do |a|
   puts "#{a + 1}"
 end
+
+=begin
+What if the block you want to pass to a method requires more logic than can fit on one line?
+It starts to become less readable and looks unwieldy.
+For multi-line blocks, the commonly accepted best practice is to change up the syntax to use do...end instead of {...}
+- The Odin project
+=end
