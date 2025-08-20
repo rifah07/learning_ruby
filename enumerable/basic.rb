7637174cd7f4f13c47f59cf8bba9adbf1fb8f2e6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-20 20:49:26 +0600

    ennumerable why? added

---
+++ enumerable/basic.rb
@@ -0,0 +1,8 @@
+=begin 
+Enumerables are a set of convenient built-in methods in Ruby that are included 
+as part of both arrays and hashes. There are some iteration patterns 
+that you’ll find yourself doing again and again, such as transforming, searching for, 
+and selecting subsets of elements in your collections.
+Enumerables were designed to make implementing these iteration patterns 
+(and therefore your life as a developer) much, much easier. - The Odin Project
+=end
