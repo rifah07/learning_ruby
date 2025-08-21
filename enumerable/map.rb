ebb7ec1b40327ca0f7f388f2b47e185619d3b84f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-21 17:18:15 +0600

    added gsub with map

--- enumerable/map.rb
+++ enumerable/map.rb
@@ -1,5 +1,17 @@
-flowers = ["rose", "lily", "tulip", "daisy", "orchid", "sunflower"]
+flowers = ["rose", "tulip", "daisy", "lily"]
 
-flowers.map { |flower| puts flower.upcase }
+plural_flowers = flowers.map { |flower| flower + "s" }
+puts plural_flowers
+# Output: ["roses", "tulips", "daisies", "lilys"]
 
-# unlike each method, map returns a new array
+puts ""
+
+plants = ["rose bush", "oak tree", "grass patch", "flower bed"]
+
+formatted_inventory = plants
+  .map { |plant| plant.gsub(" ", "_") }  # Replace spaces with underscores
+  .map.with_index { |plant, index| "#{index + 1}: #{plant.upcase}" }
+
+puts formatted_inventory
+# Output: ["1: ROSE_BUSH", "2: OAK_TREE", "3: GRASS_PATCH", "4: FLOWER_BED"]
+
