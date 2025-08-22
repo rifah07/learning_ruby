f63c4bd02924635102bc737231e054da27dbab55
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-22 22:50:44 +0600

    updated

--- enumerable/select.rb
+++ enumerable/select.rb
@@ -2,5 +2,5 @@
 responses = { 'Sharon' => 'yes', 'Leo' => 'no', 'Leila' => 'no', 'Arun' => 'yes' }
 p responses.select { |person, response| response == 'yes'}
 #=> {"Sharon"=>"yes", "Arun"=>"yes"}
-p responses.select { |_, response| response == 'yes' }
-#=> {"Sharon"=>"yes", "Arun"=>"yes"}
+p responses.select { |_, response| response == 'no' }
+#=> {"Leo"=>"no", "Leila"=>"no"}
