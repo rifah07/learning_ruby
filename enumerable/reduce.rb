888aa3d46ed9ab8b3f158e6ac6ec77bf20501a42
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-22 22:55:29 +0600

    updated

--- enumerable/reduce.rb
+++ enumerable/reduce.rb
@@ -6,3 +6,9 @@ The general idea is that it takes an array or hash and reduces it down to a sing
 nums = [3, 5, 7, 11]
 
 p nums.reduce {|sum, n| sum + n} #26
+=begin
+The first block variable in the #reduce enumerable (sum in this example) is known as the accumulator.
+The result of each iteration is stored in the accumulator and then passed to the next iteration.
+The accumulator is also the value that the #reduce method returns at the end of its work. By default,
+the initial value of the accumulator is the first element in the collection, so for each step of the iteration
+=end
