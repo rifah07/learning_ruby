739b56ff393cccb1f1d23b8051bb87f21fbcff8e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-23 13:21:12 +0600

    another exercise solved

--- enumerable/exercise.rb
+++ enumerable/exercise.rb
@@ -1,5 +1,13 @@
+# 1
 # Use each_with_index to only print out every third letter.
 
-('A'...'Z').each_with_index do |alphabet, idx|
+('A'..'Z').each_with_index do |alphabet, idx|
   puts alphabet if idx % 3 == 2
 end
+
+#2
+# Using map (and one other method), create an array that lists the numbers 0 to -100 in descending order.
+# Without typing all the numbers manually, of course.
+puts 100.times.map {|num| -num}
+# or
+puts (0..100).map {|num| -num}
