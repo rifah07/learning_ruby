4b9515337c5800e29ae2f94908e5af0e782b2431
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-24 10:47:58 +0600

    new exercise solved

--- enumerable/map_with_each_with_index.rb
+++ enumerable/map_with_each_with_index.rb
@@ -7,3 +7,17 @@ end
 
 puts "Actual array: #{arr.join(", ")}"
 puts "Mapped new array (with map): #{arr1.join("\n")}"
+
+
+# Using the array (['cat', 'goat', 'dog', 'cow']), create a new array in which
+# every second element is uppercased and backwards.
+animals = ['cat', 'goat', 'dog', 'cow']
+
+new_animals = animals.each_with_index.map do |animal , idx|
+   if idx.odd?
+      animal.upcase.reverse
+   end
+end
+
+print "New array (with map): "
+p new_animals
