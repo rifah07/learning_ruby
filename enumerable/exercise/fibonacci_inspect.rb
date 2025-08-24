8aeeafcb45b84320662f8e9652a58dc7f379639a
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-24 11:07:05 +0600

    previous solution upgraded

--- enumerable/exercise/fibonacci_inspect.rb
+++ enumerable/exercise/fibonacci_inspect.rb
@@ -2,6 +2,8 @@
 # Here's one way of creating an array with the first 20 Fibonacci numbers using a loop:
 
 arr = [0, 1]
-18.times do
-  p arr << arr[-2] + arr[-1]
+fibonacci = 18.times.inject(arr) do |fib, idx|
+  fib << fib[-2] + fib[-1]
 end
+
+puts fibonacci.join(", ")
