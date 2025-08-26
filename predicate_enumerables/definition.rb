7b51fd9b01da45b32ddbdbc4570ba9c056f0207c
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-26 16:52:47 +0600

    definition of predicate enumerable method added

---
+++ predicate_enumerables/definition.rb
@@ -0,0 +1,6 @@
+=begin
+A predicate method is indicated by a question mark (?) at the end of the method name and returns either true or false
+=end
+
+[2, 4, 6].all? { |n| n.even? }  # => true (all are even)
+[1, 2, 4].all? { |n| n.even? }  # => false (1 is odd)
