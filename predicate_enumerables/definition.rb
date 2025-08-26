74987b34e6829e929dc721d4fb957708ea88e1d2
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-26 16:56:37 +0600

    one method usage added

--- predicate_enumerables/definition.rb
+++ predicate_enumerables/definition.rb
@@ -1,5 +1,6 @@
 =begin
 A predicate method is indicated by a question mark (?) at the end of the method name and returns either true or false
+  - The Odin Project
 =end
 
 [2, 4, 6].all? { |n| n.even? }  # => true (all are even)
---
+++ predicate_enumerables/one.rb
@@ -0,0 +1,7 @@
+=begin
+Ruby has methods to check whether all, any, or none of the elements in an array match a condition.
+But what if you need exactly one match - no more, no less?
+Ruby has you covered with the #one? method,
+which returns true if the block evaluates to true for exactly one element.
+  - The Odin Project
+=end
