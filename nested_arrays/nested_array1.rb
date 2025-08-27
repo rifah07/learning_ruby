30de4d14fddf8e3fadac4669f84604cbaaef09b8
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-27 23:23:34 +0600

    another example added

--- nested_arrays/nested_array1.rb
+++ nested_arrays/nested_array1.rb
@@ -7,3 +7,20 @@ print "Array in original form (2D): "
 p math_scores
 print "Array in flattened form (1D): "
 p math_scores.flatten
+# this is how we created nested array in Ruby
+
+# another example
+friends_names = [
+  ["Alice", "Bob", "Charlie"],
+  ["David", "Eve", "Frank"],
+  ["Grace", "Heidi", "Ivan"]
+]
+
+print "Array in original form (2D): "
+p friends_names
+print "Array in flattened form (1D): "
+p friends_names.flatten
+puts "Single value : #{friends_names[0][0]}"
+puts "Single value : #{friends_names[1][2]}"
+puts "Single value : #{friends_names[1][1]}"
+puts "Single value : #{friends_names[2][2]}"
