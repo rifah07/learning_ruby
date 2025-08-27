bf03e7907f6295cc9f533c36e015a2cfef59cda2
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-27 23:20:36 +0600

    nested array example added

---
+++ nested_arrays/nested_array1.rb
@@ -0,0 +1,9 @@
+math_scores = [
+  [80, 77, 92],
+  [68, 85, 79],
+  [70, 93, 84]
+]
+print "Array in original form (2D): "
+p math_scores
+print "Array in flattened form (1D): "
+p math_scores.flatten
