f49f4b5e18bcdc4b39c1e3d3f7ec46354ff4dc9d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-08-28 11:10:30 +0600

    added immutable nested array creation example

--- nested_arrays/create_immutable.rb
+++ nested_arrays/create_immutable.rb
@@ -3,3 +3,13 @@ mutable = Array.new(3, Array.new(2))
 mutable[0][0] = 1000
 puts "The Mutable nested array: #{mutable}"
 #=> [[1000, nil], [1000, nil], [1000, nil]]
+
+immutable = Array.new(3) { Array.new(2) }
+#=> [[nil, nil], [nil, nil], [nil, nil]]
+puts "The Immutable nested array: #{immutable}"
+immutable[0][0] = 1000
+#=> 1000
+puts "The updated Immutable nested array: #{immutable}"
+#=> [[1000, nil], [nil, nil], [nil, nil]]
+# see the example, Changing the value of the first element in the first nested array
+# does not cause the value to change in any other nested array.
