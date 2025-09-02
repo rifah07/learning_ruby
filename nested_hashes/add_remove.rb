fcef268d0150ec9c984cab817e7cca2f1a60b36e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-02 20:36:42 +0600

    adding in nested showed

---
+++ nested_hashes/add_remove.rb
@@ -0,0 +1,19 @@
+vehicles = {
+  alice: { year: 2019, make: 'Toyota', model: 'Corolla' },
+  blake: { year: 2020, make: 'Volkswagen', model: 'Beetle' },
+  caleb: { year: 2020, make: 'Honda', model: 'Accord' }
+}
+puts 'The hash vehicle'
+pp vehicles
+print 'Year of Alice: '
+puts vehicles[:alice][:year]
+print 'Year of Zoe: '
+puts vehicles.dig(:zoe, :year) # nil
+
+# add new data:
+vehicles[:zoe] = { year: 2021, make: 'Ford', model: 'Escape' }
+
+puts 'After adding data'
+pp vehicles
+print 'Year of Zoe: '
+puts vehicles.dig(:zoe, :year)
