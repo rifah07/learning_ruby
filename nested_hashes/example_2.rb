d5703016b5e35ee746f185e9840ac5e5f121bcc8
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-02 19:56:21 +0600

    updated the 2nd example

--- nested_hashes/example_2.rb
+++ nested_hashes/example_2.rb
@@ -12,3 +12,6 @@ school = {
 
 puts 'The full nested array: '
 pp school
+
+puts "The students who study in Grade 9 and choose math are: #{school[:grade_9][:math]}"
+puts "The students who study in Grade 10 and studies science are: #{school[:grade_10][:science]}"
