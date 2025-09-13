b684469392a180ea72628a6acea33def8abe235b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-13 18:04:36 +0600

    class and object creation added

---
+++ oop/class.rb
@@ -0,0 +1,12 @@
+=begin
+ Ruby defines the attributes and behaviors of its objects in classes.
+ You can think of classes as basic outlines of what an object should be made of and
+  what it should be able to do.  -The Odin Project
+=end
+
+# Define class
+class GoodHorse
+end
+
+# create an object of the class
+arthur = GoodHorse.new
