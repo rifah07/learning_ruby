da77d2ce4d0c9c647a0e4dde22785edbdb619f73
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-17 00:01:01 +0600

    inheritance example code added in file

---
+++ oop/inheritance.rb
@@ -0,0 +1,21 @@
+class Plant
+  def grow
+    puts "The plant is growing."
+  end
+
+  def photosynthesize
+    puts "The plant is making food."
+  end
+end
+
+class MangoPlant < Plant
+  def bloom
+    puts "The mango plant is blooming with flowers."
+  end
+end
+
+mango = MangoPlant.new
+mango.grow # Inherited from Plant
+mango.photosynthesize # Inherited from Plant
+mango.bloom # Defined in MangoPlant# frozen_string_literal: true
+
