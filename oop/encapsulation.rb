4f6ff2fe84987f65407959b8513fdd3bb0ff02f6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-16 23:59:51 +0600

    encapsulation example code added in file

---
+++ oop/encapsulation.rb
@@ -0,0 +1,27 @@
+class BankAccount
+  def initialize(balance)
+    @balance = balance
+  end
+
+  def deposit(amount)
+    @balance += amount if amount > 0
+  end
+
+  def withdraw(amount)
+    if amount <= @balance && amount > 0
+      @balance -= amount
+    else
+      puts "Invalid withdrawal amount"
+    end
+  end
+
+  def balance
+    @balance
+  end
+end
+
+account = BankAccount.new(1000)
+account.deposit(500)
+puts account.balance # 1500
+account.withdraw(2000) # Invalid withdrawal amount# frozen_string_literal: true
+
