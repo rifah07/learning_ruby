3c8b681e91c1814d272617968b5a042444be8a72
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-18 10:31:48 +0600

    polymorphism code updated

--- oop/polymorphism.rb
+++ oop/polymorphism.rb
@@ -19,3 +19,25 @@ end
 
 animals = [Dog.new, Cat.new, Animal.new]
 animals.each { |animal| animal.speak }
+
+
+# Duck Typing Polymorphism
+
+class Duck
+  def quack
+    puts "Quack quack!"
+  end
+end
+
+class Person
+  def quack
+    puts "The person imitates a duck!"
+  end
+end
+
+def make_quack(animal)
+  animal.quack
+end
+
+make_quack(Duck.new) # Quack quack!
+make_quack(Person.new) # The person imitates a duck!
