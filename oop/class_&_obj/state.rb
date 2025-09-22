ca02444511894adcaccd1cdc6e46d1ffbc3fab56
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-22 17:35:31 +0600

    state added

---
+++ oop/class_&_obj/state.rb
@@ -0,0 +1,7 @@
+=begin
+When we define a class, we need to focus in 2 things
+state and behaviours
+State refers to the data associated to an individual object (which are tracked by instance variables).
+Behaviors are what objects are capable of doing.
+ - The Launch School E-Book
+=end
