6d46dfe7fa713ab3198b44c42dc365db2f9eae6f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-24 23:30:30 +0600

    composition added

--- oop/class_&_obj/composition.rb
+++ oop/class_&_obj/composition.rb
@@ -1,2 +1,39 @@
-# frozen_string_literal: true
+# In OOP languages, composition and aggregation are design principles
+# used to establish relationships between classes.
 
+=begin
+Composition is a relationship where an object -- often called the container --
+contains one or more objects of other classes as part of its state.
+In composition, the contained -- composed -- objects are tied directly to the container.
+That is, the lifetimes of the container and composed objects are dependent on each other.
+Composed objects are created when the container is created and destroyed when the container is destroyed.
+- The Launch School
+=end
+
+# In Ruby, composition is typically implemented using instance variables
+# that are initialized via the constructor of the class.
+
+class Engine
+  def start
+    puts "Engine Starting. Boom Boom!!!"
+  end
+end
+
+class Car
+  def initialize
+    @engine = Engine.new
+  end
+
+  def start
+    @engine.start
+  end
+end
+
+my_car = Car.new
+my_car.start
+
+=begin
+According to the Launch School:
+A container has a has-a relationship to its composed objects.
+That is, the container "has a" composed object.
+=end
