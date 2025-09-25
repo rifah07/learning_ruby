92b069581447ece82ac981dacbc50ac315020759
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-25 16:40:32 +0600

    getter method added

--- oop/class_&_obj/instance_methods.rb
+++ oop/class_&_obj/instance_methods.rb
@@ -3,6 +3,10 @@ class GoodDog
     @name = name
   end
 
+  def get_name # getter method.
+    @name
+  end
+
   def speak
     "#{@name} speaks Arf!"
   end
@@ -12,4 +16,7 @@ sparky = GoodDog.new("Sparky")
 fido = GoodDog.new("Fido")
 
 puts sparky.speak
+puts sparky.get_name
 puts fido.speak
+puts fido.get_name
+# state is different, behaviour is same
