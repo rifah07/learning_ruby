c13986fd11a8a808bf8e0648b304b179902ae749
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-25 16:46:08 +0600

    setter method added

--- oop/class_&_obj/instance_methods.rb
+++ oop/class_&_obj/instance_methods.rb
@@ -3,8 +3,12 @@ class GoodDog
     @name = name
   end
 
-  def get_name # getter method.
+  def get_name # getter method, to get the name of instance variable
     @name
+  end
+
+  def set_name=(name) # to change value of instance variable we reach for a setter method
+    @name = name
   end
 
   def speak
@@ -17,6 +21,9 @@ fido = GoodDog.new("Fido")
 
 puts sparky.speak
 puts sparky.get_name
+sparky.set_name = "Tommy"
+puts sparky.get_name
+
 puts fido.speak
 puts fido.get_name
 # state is different, behaviour is same
