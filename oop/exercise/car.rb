46c485c0103dcf8ec3bd0c26b3c0a88fac23d24e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-26 11:25:31 +0600

    1 exercise of launch school solved

---
+++ oop/exercise/car.rb
@@ -0,0 +1,38 @@
+class MyCar
+  attr_accessor :year, :color, :model
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def increase_speed(number)
+    @current_speed += number
+    puts "Speed increase: #{number}"
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You pushed brake and decreased #{number} speed"
+  end
+
+  def current_speed
+    puts "Your current speed is #{@current_speed} mph"
+  end
+  def shut_down
+    @current_speed = 0
+    puts "Now let's park!"
+  end
+end
+
+rr = MyCar.new(2000, 'Yellow', 'Rolls Royce')
+rr.increase_speed(17)
+rr.current_speed
+rr.brake(5)
+rr.current_speed
+rr.increase_speed(10)
+rr.current_speed
+rr.shut_down
+rr.current_speed
