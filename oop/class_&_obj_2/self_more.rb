d7e881b5afe4b575609af6263e0ad5a87485157b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-30 23:45:01 +0600

    concept of self updated

--- oop/class_&_obj_2/self_more.rb
+++ oop/class_&_obj_2/self_more.rb
@@ -5,3 +5,25 @@
 # Two important cases:
 #   1. Inside an instance method → `self` refers to the *calling object*.
 #   2. Inside a class but outside instance methods → `self` refers to the *class itself*.
+#
+# Rules to remember:
+# - Use `self` inside instance methods when you need to call a *setter method*
+#   (to avoid Ruby thinking you're creating a local variable).
+# - Use `self` to define *class methods*.
+
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  # ----------------------------------------
+  # INITIALIZE METHOD
+  # ----------------------------------------
+  # Here we use `self.attr=` instead of `@attr` because:
+  #   - `@name = n` assigns directly to the instance variable
+  #   - `self.name = n` calls the setter method `name=`
+  # Both achieve similar results, but when we want to be explicit about calling
+  # the setter method (especially if it has extra logic), we use `self`.
+  def initialize(n, h, w)
+    self.name   = n
+    self.height = h
+    self.weight = w
+  end
