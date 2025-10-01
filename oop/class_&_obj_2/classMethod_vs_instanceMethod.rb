1f8bf8471d10b0707abce8680b5f536ec8532903
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-10-01 23:43:05 +0600

    instance and class methods added

---
+++ oop/class_&_obj_2/classMethod_vs_instanceMethod.rb
@@ -0,0 +1,7 @@
+#===============================================
+# Understanding self, instance methods, and class methods in Ruby
+#===============================================
+
+# In Ruby, METHODS inside a class can be:
+#   * Instance methods: Called on individual objects ('instances') of a class
+#   * Class methods: Called on the class itself, NOT on its objects
