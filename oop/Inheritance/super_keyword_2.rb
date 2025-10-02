d6dcf2aaf03c9560d4968aec1be801784d32cb5f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-10-02 21:13:24 +0600

    super usage updated for initialize

---
+++ oop/Inheritance/super_keyword_2.rb
@@ -0,0 +1,28 @@
+# Another more common way of using super is with initialize
+
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "Hello."
+  end
+end
+
+class Cat < Animal
+
+  def initialize(age, name)
+    super(name)
+    @age = age
+  end
+  def speak
+    super + " My name is #{@name}! and my age is #{@age}"
+  end
+end
+
+kitty = Cat.new(2, "Kitty")
+puts kitty.speak
+
