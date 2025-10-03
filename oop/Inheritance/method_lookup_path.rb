13a600843a8c65a38a479e42340bb145ddab7bc9
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-10-04 00:21:59 +0600

    method lookup path added

---
+++ oop/Inheritance/method_lookup_path.rb
@@ -0,0 +1,35 @@
+module Walkable
+  def walk
+    "I'm walking!"
+  end
+end
+
+module Swimmable
+  def swim
+    "I'm swimming!"
+  end
+end
+
+module Climbable
+  def climb
+    "I'm climbing!"
+  end
+end
+
+class Animal
+  include Walkable
+
+  def speak
+    "This animal can speak"
+  end
+end
+
+puts "----------------Animal method lookup path----------------"
+puts Animal.ancestors
+
+=begin
+This means that when we call a method of any Animal object,
+first Ruby looks in the Animal class, then the Walkable module,
+then the Object class, then the Kernel module, and finally the BasicObject class.
+=end
+
