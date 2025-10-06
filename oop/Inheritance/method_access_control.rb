049f6f3452800c45dd96ccf8819f819c4b21304e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-10-06 16:23:26 +0600

    private method example added

---
+++ oop/Inheritance/method_access_control.rb
@@ -0,0 +1,34 @@
+=begin
+The way that Method Access Control is implemented in Ruby
+is through the use of the public, private, and protected access modifiers
+=end
+
+=begin
+We use the private method call in our program and anything below it
+is private (unless another method, like protected, is called after it to negate it)
+=end
+
+class GoodCat
+  CAT_YEARS = 7
+
+  attr_accessor :name, :age
+
+  def initialize(n , a)
+    self.name = n
+    self.age = a
+  end
+
+  private
+
+  def human_years
+    age * CAT_YEARS
+  end
+
+end
+
+kitty = GoodCat.new("Kitty", 4)
+kitty.human_years # private method 'human_years' called for an instance of GoodCat (NoMethodError)
+
+# We have made the human_years method private by placing it under the private method
+# private methods are only accessible from other methods in the class.
+
